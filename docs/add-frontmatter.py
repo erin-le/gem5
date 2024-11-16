b4504128cb7de0b91fb058a1991ad8cf49a91067
Erin Le <erinjle0@gmail.com>
2024-11-15 19:50:47 -0800

    misc: add frontmatter script

---
+++ docs/add-frontmatter.py
@@ -0,0 +1,21 @@
+import os
+
+# for filename in os.listdir('../../docs/_build/html'):
+# for filename in os.listdir('/Volumes/Crucial/gem5-dev/website/_pages/documentation/general_docs/sphinx_docs'):
+for filename in os.listdir('./_build/html'):
+    
+    print(filename)
+    if filename.startswith("gem5") and filename != "gem5.html":
+        print(filename)
+        # with open (f"../../docs/_build/html/{filename}", "r+") as f:
+        # with open (f"/Volumes/Crucial/gem5-dev/website/_pages/documentation/general_docs/sphinx_docs/{filename}", "r+") as f:
+        with open (f"./_build/html/{filename}", "r+") as f:
+            html = f.read()
+            f.seek(0,0)
+            f.write("---\n")
+            f.write(f'title: "{filename}"\n')
+            f.write("parent: sphinx-docs\n")
+            modified_filename = filename.replace("gem5.", "gem5/")
+            f.write(f"permalink: /documentation/general_docs/sphinx_docs/{modified_filename}\n")
+            f.write("---\n")
+            f.write(html)
