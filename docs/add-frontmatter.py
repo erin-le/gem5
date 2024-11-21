a951d75be24949bc21ae0ecfe89ad44bb978229f
Erin Le <ejle@ucdavis.edu>
2024-11-21 14:04:59 -0800

    misc: fix to add-frontmatter.py

--- docs/add-frontmatter.py
+++ docs/add-frontmatter.py
@@ -12,30 +12,57 @@ with open(f"./_build/html/_modules/index.html", "r+") as f:
     f.write(f"permalink: /documentation/general_docs/sphinx_docs/index.html\n")
     f.write("---\n")
     # f.write(html)
+    search_flag = False
     for line in html:
+        # remove the search bar
+        if "<search" in line:
+            search_flag = True
+        if "</search>" in line:
+            search_flag = False
+            continue
+        if search_flag == True:
+            continue
+        modified_line = None
+        # make the links at the bottom of index.html work
         if "<li><a href=" in line:
-            modified_line = (
-                line.replace("/", ".")
-                .replace("<.a>", "</a>")
-                .replace("<.li>", "</li>")
-            )
-            f.write(modified_line)
-        elif (
-            line == '<h1 class="logo"><a href="../index.html">gem5</a></h1>'
-            or line
-            == '<li class="toctree-l1"><a class="reference internal" href="../gem5.html">gem5 package</a></li>'
-        ):
-            modified_line = line.replace("../", "./")
-            f.write(modified_line)
+            if (
+                '<li><a href="../index.html">Documentation overview</a><ul>'
+                in line
+            ):
+                modified_line = (
+                    '<li><a href="./index.html">Documentation overview</a><ul>'
+                )
+            else:
+                modified_line = (
+                    line.replace("/", ".")
+                    .replace("<.a>", "</a>")
+                    .replace("<.li>", "</li>")
+                )
         else:
-            modified_line = line
+            if (
+                '<li class="toctree-l1"><a class="reference internal" href="../gem5.html">gem5 package</a></li>'
+                in line
+            ):
+                print("../gem5.html switched to ./gem5.html")
+                modified_line = '<li class="toctree-l1"><a class="reference internal" href="./gem5.html">gem5 package</a></li>'
+
+            elif (
+                '<h1 class="logo"><a href="../index.html">gem5</a></h1>'
+                in line
+            ):
+                print("../index.html switched to ./index.html")
+                modified_line = (
+                    '<h1 class="logo"><a href="./index.html">gem5</a></h1>'
+                )
+            else:
+                modified_line = line
         f.write(modified_line)
 
 for filename in os.listdir("./_build/html"):
 
-    print(filename)
+    # print(filename)
     if filename.startswith("gem5"):  # and filename != "gem5.html"
-        print(filename)
+        # print(filename)
         # with open (f"../../docs/_build/html/{filename}", "r+") as f:
         # with open (f"/Volumes/Crucial/gem5-dev/website/_pages/documentation/general_docs/sphinx_docs/{filename}", "r+") as f:
         with open(f"./_build/html/{filename}", "r+") as f:
