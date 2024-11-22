782d2afb85a35ddf3ae434dd3b3bec19d02da51f
Erin Le <ejle@ucdavis.edu>
2024-11-21 17:31:05 -0800

    misc: add line to remove weird leftover search bar line

--- docs/add-frontmatter.py
+++ docs/add-frontmatter.py
@@ -59,7 +59,13 @@ with open(f"./_build/html/_modules/index.html", "r+") as f:
                 elif search_flag == True:
                     modified_line = ""
                 else:
-                    modified_line = line
+                    if (
+                        '</html>searchbox\').style.display = "block"</script>'
+                        in line
+                    ):
+                        modified_line = "</html>"
+                    else:
+                        modified_line = line
         f.write(modified_line)
         print("latest version gotten")
 for filename in os.listdir("./_build/html"):
